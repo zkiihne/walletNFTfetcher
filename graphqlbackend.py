e6344954f3d6b5d48a4ee8cc31e58377c265b18a
Zachary Kiihne <zkiihne@gmail.com>
2021-03-26 21:35:08 -0400

    Updated to add parallelization to requests

--- graphqlbackend.py
+++ graphqlbackend.py
@@ -3,15 +3,116 @@ import urllib.request
 from PIL import Image
 import json
 import requests
+from urllib.request import Request, urlopen  # Python 3
 
-# helper to actually fetch the NFTs of a wallet
-def get_uri(contract_address, token_id, owner_address):
-    import warnings
+# # helper to actually fetch the NFTs of a wallet
+# def get_uri(contract_address, token_id, owner_address):
+#     import warnings
 
-    warnings.filterwarnings("ignore", category=DeprecationWarning)
+#     warnings.filterwarnings("ignore", category=DeprecationWarning)
+#     from web3 import Web3
+#     w3 = Web3(Web3.HTTPProvider("https://cloudflare-eth.com"))
+
+#     simplified_abi = [
+#         {
+#             'inputs': [{'internalType': 'address', 'name': 'owner', 'type': 'address'}],
+#             'name': 'balanceOf',
+#             'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}],
+#             'payable': False, 'stateMutability': 'view', 'type': 'function', 'constant': True
+#         },
+#         {
+#             'inputs': [],
+#             'name': 'name',
+#             'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}],
+#             'stateMutability': 'view', 'type': 'function', 'constant': True
+#         },
+#         {
+#             'inputs': [{'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256'}],
+#             'name': 'ownerOf',
+#             'outputs': [{'internalType': 'address', 'name': '', 'type': 'address'}],
+#             'payable': False, 'stateMutability': 'view', 'type': 'function', 'constant': True
+#         },
+#         {
+#             'inputs': [{'internalType': 'uint256', 'name': 'tokenId', 'type': 'uint256'}],
+#             'name': 'tokenURI',
+#             'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}],
+#             'payable': False, 'stateMutability': 'view', 'type': 'function', 'constant': True
+#         },
+#         {
+#             'inputs': [],
+#             'name': 'symbol',
+#             'outputs': [{'internalType': 'string', 'name': '', 'type': 'string'}],
+#             'stateMutability': 'view', 'type': 'function', 'constant': True
+#         },
+#         {
+#             'inputs': [],
+#             'name': 'totalSupply',
+#             'outputs': [{'internalType': 'uint256', 'name': '', 'type': 'uint256'}],
+#             'stateMutability': 'view', 'type': 'function', 'constant': True
+#         },
+#     ]
+
+    
+#     image_links = []
+#     total_uri = []
+#     for i in range(0, len(token_id)):
+#         ca = contract_address[i]
+#         ti = token_id[i]
+#         ck_contract = w3.eth.contract(address=w3.toChecksumAddress(ca), abi=simplified_abi + ck_extra_abi)
+#         try:
+#             name = ck_contract.functions.name().call()
+#             symbol = ck_contract.functions.symbol().call()
+#             uri = ck_contract.functions.tokenURI(ti).call()
+#             owner = ck_contract.functions.ownerOf(ti).call()
+#             print(name, symbol, owner, uri)
+
+#             if owner.lower() == owner_address.lower():
+#                 x = requests.get(uri)
+#                 # print(x.json())
+#                 ipfsurl = 'https://ipfs.io/ipfs/'
+#                 imageurl = x.json().get("image")
+#                 if 'ipfs://' in imageurl:
+#                     imageurl = ipfsurl + imageurl.split("ipfs://ipfs/")[1]
+
+#                     try:
+#                         image = Image.open(urllib.request.urlopen(imageurl, timeout=0.5))
+#                         width, height = image.size
+#                         width = int(width / 1000)
+#                         height = int(height / 1000)
+#                         if width == 0 or height == 0:
+#                             width = width + 1
+#                             height = height + 1
+
+#                         # print(width, height)
+#                         image_links.append([imageurl, width, height])
+#                         total_uri.append(x.json())
+#                     except Exception as e:
+#                         print(e)
+#                 else:
+#                     try:
+#                         image = Image.open(urllib.request.urlopen(imageurl, timeout=0.5))
+#                         width, height = image.size
+#                         width = int(width/1000)
+#                         height = int(height / 1000)
+#                         if width == 0 or height == 0:
+#                             width = width +1
+#                             height = height +1
+
+#                         # print(width, height)
+#                         image_links.append([imageurl, width, height])
+#                         total_uri.append(x.json())
+#                     except Exception as e:
+#                         print(e)
+#         except Exception as e:
+#             print(e)
+#     return total_uri, image_links
+
+def threadfetch(inp):
+    ca = inp[0]
+    ti = inp[1]
+    owner_address = inp[2]
     from web3 import Web3
     w3 = Web3(Web3.HTTPProvider("https://cloudflare-eth.com"))
-
     simplified_abi = [
         {
             'inputs': [{'internalType': 'address', 'name': 'owner', 'type': 'address'}],
@@ -52,58 +153,99 @@ def get_uri(contract_address, token_id, owner_address):
     ]
 
     
+    ck_contract = w3.eth.contract(address=w3.toChecksumAddress(ca), abi=simplified_abi)
+    try:
+        name = ck_contract.functions.name().call()
+        symbol = ck_contract.functions.symbol().call()
+        uri = ck_contract.functions.tokenURI(ti).call()
+        owner = ck_contract.functions.ownerOf(ti).call()
+
+        if owner.lower() == owner_address.lower() or owner_address == "":
+            print("chimp", name, symbol, owner, uri)
+            ipfsurl = 'https://ipfs.io/ipfs/'
+            if 'ipfs://' in uri:
+                uri = ipfsurl + uri.split("ipfs://ipfs/")[1]
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+            x = requests.get(uri, headers=headers)
+            xjson = x.json()
+
+            imageurl = x.json().get("image")
+            uriresult = None
+            imageresult = None
+            if 'ipfs://' in imageurl:
+                imageurl = ipfsurl + imageurl.split("ipfs://ipfs/")[1]
+
+                try:
+                    req = Request(imageurl)
+                    req.add_header('User-Agent', headers.get('User-Agent'))
+                    image = Image.open(urlopen(req, timeout=5))
+
+                    width, height = image.size
+                    xjson["height"] = height
+                    xjson["width"] = width
+
+                    if width == 0 or height == 0:
+                        width = width + 1
+                        height = height + 1
+
+                    # print(width, height)
+                    imageresult = [imageurl, width, height]
+                    uriresult = xjson
+                except:
+                    pass
+                    # traceback.print_exc()
+            else:
+                try:
+                    req = Request(imageurl)
+                    req.add_header('User-Agent', headers.get('User-Agent'))
+                    image = Image.open(urlopen(req, timeout=5))
+                    width, height = image.size
+                    xjson["height"] = height
+                    xjson["width"] = width
+
+                    if width == 0 or height == 0:
+                        width = width + 1
+                        height = height + 1
+
+                    # print(width, height)
+                    imageresult = [imageurl, width, height]
+                    uriresult = xjson
+                except:
+                    pass
+                    # traceback.print_exc()
+            if uriresult != None:
+                return [uriresult, imageresult]
+    except Exception as e:
+        pass
+        # print(e)
+
+
+# helper to actually fetch the NFTs of a wallet
+from multiprocessing.dummy import Pool as ThreadPool
+
+def get_uri(contract_address, token_id, owner_address):
+    import warnings
+
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+
     image_links = []
     total_uri = []
-    for i in range(0, len(token_id)):
-        ca = contract_address[i]
-        ti = token_id[i]
-        ck_contract = w3.eth.contract(address=w3.toChecksumAddress(ca), abi=simplified_abi + ck_extra_abi)
-        try:
-            name = ck_contract.functions.name().call()
-            symbol = ck_contract.functions.symbol().call()
-            uri = ck_contract.functions.tokenURI(ti).call()
-            owner = ck_contract.functions.ownerOf(ti).call()
-            print(name, symbol, owner, uri)
-
-            if owner.lower() == owner_address.lower():
-                x = requests.get(uri)
-                # print(x.json())
-                ipfsurl = 'https://ipfs.io/ipfs/'
-                imageurl = x.json().get("image")
-                if 'ipfs://' in imageurl:
-                    imageurl = ipfsurl + imageurl.split("ipfs://ipfs/")[1]
-
-                    try:
-                        image = Image.open(urllib.request.urlopen(imageurl, timeout=0.5))
-                        width, height = image.size
-                        width = int(width / 1000)
-                        height = int(height / 1000)
-                        if width == 0 or height == 0:
-                            width = width + 1
-                            height = height + 1
-
-                        # print(width, height)
-                        image_links.append([imageurl, width, height])
-                        total_uri.append(x.json())
-                    except Exception as e:
-                        print(e)
-                else:
-                    try:
-                        image = Image.open(urllib.request.urlopen(imageurl, timeout=0.5))
-                        width, height = image.size
-                        width = int(width/1000)
-                        height = int(height / 1000)
-                        if width == 0 or height == 0:
-                            width = width +1
-                            height = height +1
-
-                        # print(width, height)
-                        image_links.append([imageurl, width, height])
-                        total_uri.append(x.json())
-                    except Exception as e:
-                        print(e)
-        except Exception as e:
-            print(e)
+    inputarray = []
+    for i in range(0, len(contract_address)):
+        inputarray.append([contract_address[i], int(token_id[i]), owner_address])
+    pool = ThreadPool(len(contract_address))
+    # for i in range(0, len(token_id)):
+    #     ca = contract_address[i]
+    #     ti = int(token_id[i])
+    results = pool.map(threadfetch, inputarray)
+    print(results)
+    for r in results:
+        if r != None:
+            total_uri.append(r[0])
+            image_links.append(r[1])
     return total_uri, image_links
 
 # gets all NFTs for a given address
